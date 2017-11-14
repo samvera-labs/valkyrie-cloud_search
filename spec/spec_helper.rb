f3af4fe92406b9819d4f25084492cd6206475617
Michael Klein <mbklein@gmail.com>
2017-11-14 16:36:34 -0600

    Add Coveralls.wear!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV['RACK_ENV'] = 'test'
 ENV['RAILS_ENV'] = 'test'
 require 'simplecov'
 require 'coveralls'
+Coveralls.wear!
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
   [
     SimpleCov::Formatter::HTMLFormatter,
